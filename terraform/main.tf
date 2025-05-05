ae39239b122d146bd51596317ec1e55e9b2880c0
Pavel Kondratovich <kondratovich@ro.ru>
2025-05-05 17:23:09 +0300

    upd

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,24 @@
-resource "azurerm_resource_group" "example" {
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+
+  required_version = ">= 1.6.0"
+}
+
+provider "azurerm" {
+  features {}
+
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
+}
+
+resource "azurerm_resource_group" "main" {
   name     = "rg-devops-basic"
   location = "West Europe"
 }
