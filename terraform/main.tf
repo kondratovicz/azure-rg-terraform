a0aa40a2d22e250a1a60287601faeab69f756f73
Pavel Kondratovich <kondratovich@ro.ru>
2025-05-07 17:12:57 +0300

    fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,7 +11,7 @@ terraform {
     resource_group_name  = "resource_group_devops"
     storage_account_name = "othernamesalreadytaken"
     container_name       = "tfstate"
-    key                  = "${GITHUB_WORKSPACE}-terraform.tfstate"
+    key = "${terraform.workspace}-terraform.tfstate"
   }
 }
 
