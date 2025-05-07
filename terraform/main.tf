41770e795d37cee1399b8d3e2e7c293fa4c1531f
Pavel Kondratovich <kondratovich@ro.ru>
2025-05-07 19:49:27 +0300

    fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,10 +17,10 @@ resource "tls_private_key" "ssh_key" {
 provider "azurerm" {
   features {}
 
-  #client_id       = var.client_id
-  #client_secret   = var.client_secret
-  #tenant_id       = var.tenant_id
-  #subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
 }
 
 resource "azurerm_resource_group" "example" {
